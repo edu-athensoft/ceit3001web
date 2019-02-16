b2932f3192158207faf7e19df9e27d89fc83c300
lada314 <lada314@gmail.com>
2019-02-15 22:26:13 -0500

    added striped table

---
+++ demo/main.js
@@ -0,0 +1,14 @@
+/* print a sentence on screen */
+
+function sayHello(){
+    alert("hello world!");
+}
+
+
+function sayHelloTo(name){
+    alert("hello world! "+name);
+}
+
+function youFindSecret(){
+    alert("bingo");
+}
