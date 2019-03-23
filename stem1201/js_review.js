84b2f76f47a6d1e73b7e5e7ac2e4773b3f09860a
lada314 <lada314@gmail.com>
2019-03-23 10:04:45 -0400

    added datatype

---
+++ stem1201/js_review.js
@@ -0,0 +1,65 @@
+/* review */
+//dynamic language
+//static language
+
+//identifier
+
+
+//statement
+//1. declared a variable
+var a ;
+
+//2. initialized a variable
+a = 0;
+
+var a = 0;
+
+var name = 0;      //integer, number
+name = 'myname';   //string
+name = true;       // boolean
+
+name = a;
+name = b;
+
+//operator
+//arithmatic operator
+// + , - , * ,/ , %, 
+// && || !  ^
+// and  or   not   xor
+// &  |   ~
+
+// assignment operator
+// =
+// +=, -=, *=, /* ,  %= 
+var b = 9;   //assignment statement
+var c = 3;
+var d = 5;
+
+b += 1;   // b = b+ 1;
+c -= 2;   // c = c -2;
+d *= 3;   // d = d * 3;
+
+//comparison 
+// >, < ,  ==,  ===,  >=, <=, !=, 
+
+
+//expression
+//logic expression
+// true && fasle
+
+//comparing expression
+// a < b 
+
+
+
+
+//Data types
+
+
+
+
+
+
+
+
+
